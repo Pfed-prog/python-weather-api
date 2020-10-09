532c79f4c7443a0a6aba1d1f673a9947c44f4357
Pfed-prog <fedorovn19@gmail.com>
2020-10-09 18:26:58 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import urllib.request
+import json
+
+def api(my_api):
+    print('Enter latitude:')
+    lat = input()
+    print('Enter longtitude:')
+    lon=input()
+    url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&units=metric&exclude=minutely,current,hourly,alerts&appid={}".format(l, lo, my_api)
+    json_obj = urllib.request.urlopen(url)
+    data = json.load(json_obj)
+    x=0
+    for i in data['daily'][:6]:
+        print('day', x,'. Morn:', i['temp']['morn'],'; Max:',i['temp']['max'])
+        x+=1
+api(my_api)
